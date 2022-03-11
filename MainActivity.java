3379875e75e1a629570c4c4ee292fcadfc746872
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-03-11 22:16:58 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Toast;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity
 {
     private Integer counter = 0;
-    private Integer counter2 = 10;
-    public static final String TAG = "StartActivity";
+    private Integer digitOfNumberOfCounter = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,42 +17,63 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView counterView = findViewById(R.id.txt_counter);
+        ViewGroup.LayoutParams params = counterView.getLayoutParams();
         if (savedInstanceState != null && savedInstanceState.containsKey("counter"))
         {
             counter = savedInstanceState.getInt("counter");
         }
-        counterView.setText(counter.toString());        
+        if (savedInstanceState != null && savedInstanceState.containsKey("counter"))
+        {
+            digitOfNumberOfCounter = savedInstanceState.getInt("counter2");
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey("height"))
+        {
+            params.height = savedInstanceState.getInt("height");
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey("width"))
+        {
+            params.width = savedInstanceState.getInt("width");
+        }
+        counterView.setLayoutParams(params);
+        counterView.setText(counter.toString());
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState)
     {
+        TextView counterView = findViewById(R.id.txt_counter);
+        ViewGroup.LayoutParams params = counterView.getLayoutParams();
         super.onRestoreInstanceState(savedInstanceState);
         counter = savedInstanceState.getInt("counter");
+        digitOfNumberOfCounter = savedInstanceState.getInt("counter2");
+        params.height = savedInstanceState.getInt("height");
+        params.width = savedInstanceState.getInt("width");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState)
     {
+        TextView counterView = findViewById(R.id.txt_counter);
+        ViewGroup.LayoutParams params = counterView.getLayoutParams();
         super.onSaveInstanceState(outState);
         outState.putInt("counter", counter);
+        outState.putInt("counter2", digitOfNumberOfCounter);
+        outState.putInt("height", params.height);
+        outState.putInt("width", params.width);
     }
 
-    public void onClickBtnAddStudents(View view)
+    public void onClickPassTheMeter(View view)
     {
-        counter++;
         TextView counterView = findViewById(R.id.txt_counter);
-        if(counter/10 == counter2)
+        ViewGroup.LayoutParams params = counterView.getLayoutParams();
+        counter++;
+        if(counter/10 == digitOfNumberOfCounter)
         {
-            TextView layout = findViewById(R.id.txt_counter);
-            ViewGroup.LayoutParams params = layout.getLayoutParams();
             params.width += 20;
             params.height += 20;
-            layout.setLayoutParams(params);
-            counter2 *= 10;
+            counterView.setLayoutParams(params);
+            digitOfNumberOfCounter *= 10;
         }
         counterView.setText(counter.toString());
-        counterView.setWidth(150);
-        counterView.setHeight(150);
     }
 }
